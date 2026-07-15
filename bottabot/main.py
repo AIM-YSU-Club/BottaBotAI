9abfdb0f5fcf36d5424ea70d655b218649c9870a
JasontreksC <jasontreks@outlook.com>
2026-07-15 15:18:58 +0900

    chore: 필요하지 않은 embed_api는 삭제

--- bottabot/main.py
+++ bottabot/main.py
@@ -1,13 +1,13 @@
 # FastAPI 서버 구현부
-import asyncio
-from fastapi import FastAPI, Request
-# API 라우터
-from bottabot.api.embed_api import embed_api_router 
+from fastapi import FastAPI
+
+from bottabot.api.chat_api import chat_api_router
 from bottabot.api.docparse_api import docparse_api_router
 
 app = FastAPI(title="BottaBotAI")
-app.include_router(embed_api_router)
 app.include_router(docparse_api_router)
+app.include_router(chat_api_router)
+
 
 @app.get("/")
 async def read_root():
